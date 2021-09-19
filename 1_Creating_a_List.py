4522d066c72dcab3a95ade359a32161b310e53c2
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-19 19:19:24 +0530

    Create 1_Creating_a_List.py

---
+++ 1_Creating_a_List.py
@@ -0,0 +1,13 @@
+### 1. Let's create a List
+
+List1 = [1,2,3,4,5]
+print(List1)
+
+List2 = ["Apple","Banana","Mangoes","Oranges"]
+print(List2)
+
+List3 = [1,2,3,"Mangoes","Oranges"]
+print(List3)
+
+List4 = [1,2,3,["Apples", "Banana"],[1.2,2.6]]
+print(List4)
