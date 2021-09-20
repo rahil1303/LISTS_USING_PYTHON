706c5dc1918ef95ed20c582d67ffa6333963bfb5
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-20 13:48:04 +0530

    Create 6_List_Operations.py

---
+++ 6_List_Operations.py
@@ -0,0 +1,19 @@
+### "+" operator concatenates
+a = [1,2,3]
+b = [4,5,6]
+c = a+b
+print(c)
+
+### "*" operator 
+a = [0]
+a = a*4
+print(a)'
+
+### len() function of lists
+print(len(c))
+
+### Max() returns the item with the highest value in the List 
+print(max(c))
+
+### Sum() returns sum of all items in the List
+print(sum(c))
