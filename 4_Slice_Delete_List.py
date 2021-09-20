7ff8974991dab7c2d334393da9ab80f1e0dbc638
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-20 12:30:05 +0530

    Create 4_Slice_Delete_List.py

---
+++ 4_Slice_Delete_List.py
@@ -0,0 +1,18 @@
+### Delete an Element from list:
+# 1. Pop() Method
+# 2. Delete() Method
+# 3. Remove() Method
+
+
+# Pop Method
+myList4 = ['a','b','c','d','e','f']
+myList4.pop(1)
+print(myList4)
+
+# Delete Method
+del myList4[1]
+print(myList4)
+
+# Remove Method
+myList4.remove("d")
+print(myList4)
